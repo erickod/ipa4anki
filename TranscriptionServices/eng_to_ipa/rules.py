188d55ad4360bd866d7e0e199941f94df36761e1
Erick Duarte <erickod@gmail.com>
2019-09-18 08:50:31 -0300

    Fix the preferred transcriptions to the last word of a list and a single word transcription

--- TranscriptionServices/eng_to_ipa/rules.py
+++ TranscriptionServices/eng_to_ipa/rules.py
@@ -26,23 +26,37 @@ def changes_when_next_word_starts_with_a_consonant(ipa_list) -> 'ipa_list':
             current_element[0] = symbols_sub[current_element[0]]
     return ipa_list
 
+def changes_when_it_is_just_one_word(ipa_list: list) -> 'ipa_list':
+    symbols_sub = {
+        "eɪ":"ʌ"
+    }
+    if len(ipa_list) > 1 and len(ipa_list[0]) > 1 and ipa_list[0][0] in symbols_sub.keys():
+        ipa_list[0][0] = symbols_sub[ipa_list[0][0]]
+    return ipa_list
+
 
 def changes_when_exists_on_preferred_transcriptions(ipa_list: list) -> 'ipa_list':
     symbols_sub = {
         'hwaɪ':'waɪ', "bɑlm": "bɑm", "hwʌt": "wʌt" , "dʒɪst": "dʒʌst",
-        "frər": 'fɔr', "gɪd": "gʊd", "hwʌn":"wʌn",
+        "frər": 'fɔr', "gɪd": "gʊd", "hwʌn":"wʌn", "hwɛn":"wɛn"
         }
-    max_index = len(ipa_list) -1
+    """max_index = len(ipa_list) -1
 
     for index in range(max_index):
         current_element = ipa_list[index]
-        next_element = ipa_list[index + 1] if index < max_index else None
+        next_element = ipa_list[index + 1] if index == max_index else None
 
         if current_element[0] in symbols_sub:
-            current_element[0] = symbols_sub[current_element[0]]
+            current_element[0] = symbols_sub[current_element[0]]"""
+
+    for ipa_el in ipa_list:
+        ipa_el_index = ipa_list.index(ipa_el)
+        if ipa_el[0] in symbols_sub.keys():
+            ipa_list[ipa_el_index][0] = symbols_sub[ipa_el[0]]
     return ipa_list
 
 def make_transcription_changes(ipa_list: list,) -> str:
+    ipa_list = changes_when_it_is_just_one_word(ipa_list)
     ipa_list = changes_when_next_word_starts_with_a_consonant(ipa_list)
     ipa_list = changes_when_exists_on_preferred_transcriptions(ipa_list)
     return ipa_list
